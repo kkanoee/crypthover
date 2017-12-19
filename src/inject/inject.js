634ddf0c7090181aa57dae5d19a7bd37f4d01a98
kkanoee <31211058+kkanoee@users.noreply.github.com>
2017-12-19 18:53:37 +0100

    Update inject.js
    L16 : Ajout de l'url vers coinmarketcap
    
    L30 : ajout de toUpperCase();

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,5 +1,4 @@
 var CRYPTOS_DATA = {};
-var FOCUS_ON_TOOLTIP = false;
 
 const intlFormatter = new Intl.NumberFormat('en-US', {
   style: 'currency',
@@ -10,24 +9,25 @@ const intlFormatter = new Intl.NumberFormat('en-US', {
 const getHtmlTemplate = function(currency, price) {
   console.log(currency);
 
-  const url = `https://www.cryptocompare.com/coins/${currency.Name}/overview`;
-
   return `
     <p>
         <h2><img src="https://www.cryptocompare.com${currency.ImageUrl}" width="30" /> ${currency.CoinName}</h2>
     </p>
+			<a href="https://coinmarketcap.com/currencies/${currency.CoinName}" target="_top">CoinMarketCap</a>
     <p>
-        ${price.EUR}€<br />
+        <br />
+
+		<br />
         ${intlFormatter.format(price.USD)}
     </p>
-    <p><a class="crypto-website" href="${url}" target="_blank">See on cryptocompare</a></p>
+    <p>Total coins supply ${intlFormatter.format(currency.TotalCoinSupply)}</p>
   `
 }
 
 // Functions
 const displayTooltipOnCashtag = function(event, template) {
   console.log('event', event.target.innerHTML);
-  const currencyTag = event.target.innerHTML;
+  const currencyTag = event.target.innerHTML.toUpperCase();
 
   if (!CRYPTOS_DATA[currencyTag]) {
     return;
@@ -35,24 +35,20 @@ const displayTooltipOnCashtag = function(event, template) {
 
   const currency = CRYPTOS_DATA[currencyTag];
   event.target.style.cursor = "progress";
-  fetch(`https://min-api.cryptocompare.com/data/price?fsym=${currencyTag}&tsyms=USD,EUR`)
+  fetch(`https://min-api.cryptocompare.com/data/price?fsym=${currencyTag}&tsyms=USD`)
     .then(response => response.json())
     .then(price => {
       event.target.style.cursor = "auto";
 
       template.innerHTML = getHtmlTemplate(currency, price);
       template.style.visibility = "visible";
-      template.style.top = `${event.pageY-10}px`;
-      template.style.left = `${event.pageX-10}px`;
+      template.style.top = `${event.pageY}px`;
+      template.style.left = `${event.pageX}px`;
     })
 }
 
-const hideTooltip = function(event, template, timeout = 0) {
-  setTimeout(() => {
-    if (!FOCUS_ON_TOOLTIP) {
-      template.style.visibility = "hidden";
-    }
-  }, timeout);
+const hideTooltip = function(event, template) {
+  template.style.visibility = "hidden";
 }
 
 const liveListener = function(eventType, elementQuerySelector, cb) {
@@ -89,15 +85,6 @@ chrome.extension.sendMessage({}, function (response) {
       template.className = "crypthover"
       document.body.appendChild(template);
 
-      template.addEventListener("mouseover", () => {
-        FOCUS_ON_TOOLTIP = true;
-        console.log(FOCUS_ON_TOOLTIP);
-      })
-      template.addEventListener("mouseout", () => {
-        FOCUS_ON_TOOLTIP = false;
-        hideTooltip(event, template);
-      })
-
       // Get crypto datas
       fetch('https://min-api.cryptocompare.com/data/all/coinlist')
         .then(response => response.json())
@@ -106,7 +93,7 @@ chrome.extension.sendMessage({}, function (response) {
 
           // Add event listener
           liveListener("mouseover", ".twitter-cashtag", (event) => displayTooltipOnCashtag(event, template))
-          liveListener("mouseout", ".twitter-cashtag", (event) => hideTooltip(event, template, 500))
+          liveListener("mouseout", ".twitter-cashtag", (event) => hideTooltip(event, template))
         })
     }
   }, 10);
